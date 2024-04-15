4cba25cd43d13c0fab29fff931159f0113d4ad56
Arduino-05 <larduinobatista@gmail.com>
2024-04-15 08:07:48 -0300

    API final

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
 from flask import Flask, jsonify, request
+from randan_data import data
+import funcoes
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=("GET", ))
 def index():
     return jsonify({"status":200, "message": "API Do Luiz_Gustavo_Arduino_Batista"})
 
-@app.route("/aleatorios")
+@app.route("/aleatorios", methods=("GET", ))
 def aleatorios():
     import random
     a = random.randint(49, 100)
     return jsonify({"status": 200, "data": a })
 
-@app.route("/argumentos/<string:nome>")
+@app.route("/argumentos/<string:nome>", methods=("GET", ))
 def argumento(nome: str):
     return jsonify({"status": 200, "data": nome})
 
-@app.route("/argumentos")
+@app.route("/argumentos", methods=("GET", ))
 def arg_implicito():
     return jsonify({"status": 200, "data": request.args["nome"]})
+
+@app.route("/idades", methods=("GET", ))
+def idades():
+    num = funcoes.maiores(data)
+    return jsonify({"status": 200, "data": num})
+
+@app.route("/salario", methods=("GET", ))
+def salario():
+    salario = funcoes.salario(data)
+    return jsonify({"status": 200,"data": salario})
+
+@app.route("/maior_salario", methods=("GET", ))
+def max():
+    max = funcoes.maxsalarios(data)
+    return jsonify({"status": 200, "data": max})
+
+@app.route("/profissoes", methods=("GET", ))
+def profissoes():
+    profissoes = funcoes.media_profissao(data)
+    return jsonify({"status": 200, "data":profissoes})
+
+@app.route("/idades/sexo=true")
+def sexo():
+    sexo = funcoes.sexoidade(data)
+    return jsonify({"status":200, "data":sexo})
