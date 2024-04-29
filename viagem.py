fe6ba19b15c820b641b500e906b533a53cfb23b0
Arduino-05 <larduinobatista@gmail.com>
2024-04-29 14:42:21 -0300

    Pedindo nome para o usuário

---
+++ viagem.py
@@ -0,0 +1,7 @@
+V = input("Digite a velocidade:")
+M = input("Digite o nome do motorista:")
+D = input("Digite a Distancia:")
+V = int (D)
+D = int (V)
+T = D/V
+msg = input(f"O tempo para percorrer {D} em {V}KM e de {T}H com o motorista {M}")
